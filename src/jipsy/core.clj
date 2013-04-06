49cbed1df3462ef62ab3f00ce11bc2e21fe21878
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-06 14:33:34 -0700

    Add some EBNF

--- src/jipsy/core.clj
+++ src/jipsy/core.clj
@@ -4,3 +4,7 @@
   `(let [x# ~x]
      (println "debug:" '~x "=" x#)
      x#))
+
+;; factor = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
+;; mathop = "+" | "-" | "*" | "/" ;
+;; expression = factor | "(" , mathop , expression , { expression } , ")" ;
