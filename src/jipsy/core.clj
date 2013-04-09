fd2b26b678186b3faf7ba3ff74921bdf63961bdb
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-09 14:40:27 -0700

    Add a parser generated by instaparse

--- src/jipsy/core.clj
+++ src/jipsy/core.clj
@@ -12,6 +12,13 @@
 ;; expression = factor
 ;;            | "(" , mathop , space , expression , { space , expression } , ")" ;
 
+(def parser
+  (instaparse/parser
+    "expression = factor | '(' mathop space expression { space expression } ')'
+     factor = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+     mathop = '+' | '-' | '*' | '/'
+     space = ' '"))
+
 (def factor-regex #"^[0-9]$")
 (def mathop-regex #"^[\+\-\*\/]$")
 
