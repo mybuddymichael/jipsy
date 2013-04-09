28ab71ab01e2d015f2f9c206b38c8f3b14366858
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-09 15:31:48 -0700

    Make commas whitespace

--- src/jipsy/core.clj
+++ src/jipsy/core.clj
@@ -12,4 +12,4 @@
                 | <'('> mathop <whitespace> expression { <whitespace> expression } <')'>
      factor = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
      mathop = '+' | '-' | '*' | '/'
-     whitespace = #'\\s+'"))
+     whitespace = #'[\\s,]+'"))
