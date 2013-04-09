09a6897e8546a3c71449024a26cbcb02c75c9bec
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-09 16:52:09 -0700

    Separate factor from digits

--- src/jipsy/core.clj
+++ src/jipsy/core.clj
@@ -8,8 +8,9 @@
 
 (def parser
   (instaparse/parser
-    "expression = factor+
+    "expression = factor
                 | <'('> mathop <whitespace> expression { <whitespace> expression } <')'>
-     factor = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+     factor = digit+
+     digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
      mathop = '+' | '-' | '*' | '/'
      whitespace = #'[\\s,]+'"))
