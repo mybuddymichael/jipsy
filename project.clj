5878843c8065ec3735dbcb95e739af16dcb7ecbe
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-09 13:35:50 -0700

    Add instaparse as a dep

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/mybuddymichael/jipsy"
   :license {:name "GNU LGPLv3"
             :url "http://www.gnu.org/licenses/lgpl-3.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [instaparse "1.0.0"]])
