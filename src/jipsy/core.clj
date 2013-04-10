83de786b0dabd5a411832aa6af3f16d44d269d2f
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-09 22:41:53 -0700

    Hide digit tags

--- src/jipsy/core.clj
+++ src/jipsy/core.clj
@@ -11,6 +11,6 @@
     "expression = factor
                 | <'('> mathop <whitespace> expression { <whitespace> expression } <')'>
      factor = digit+
-     digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+     <digit> = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
      mathop = '+' | '-' | '*' | '/'
      whitespace = #'[\\s,]+'"))
