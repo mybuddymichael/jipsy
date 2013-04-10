69f237ef4c235b4fe5a6320739a46a3c9ea496df
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-09 22:49:39 -0700

    Extract the grammar string into a var

--- src/jipsy/core.clj
+++ src/jipsy/core.clj
@@ -6,13 +6,14 @@
      (println "debug:" '~x "=" x#)
      x#))
 
-(def parse
-  (instaparse/parser
-    "expression = factor
-                | name
-                | <'('> mathop <whitespace> expression { <whitespace> expression } <')'>
-     factor = digit+
-     <digit> = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-     mathop = '+' | '-' | '*' | '/'
-     name = #'[A-Za-z][A-Za-z0-9_]*'
-     whitespace = #'[\\s,]+'"))
+(def grammar
+  "expression = factor
+   | name
+   | <'('> mathop <whitespace> expression { <whitespace> expression } <')'>
+   factor = digit+
+   <digit> = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+   mathop = '+' | '-' | '*' | '/'
+   name = #'[A-Za-z][A-Za-z0-9_]*'
+   whitespace = #'[\\s,]+'")
+
+(def parse (instaparse/parser grammar))
