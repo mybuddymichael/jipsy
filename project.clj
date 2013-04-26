fc1d9a9a061689ce4d22111e15b890836f582e2b
Michael Hanson <michael.b.hanson@gmail.com>
2013-04-25 20:47:26 -0700

    Instaparse 1.0.1

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "GNU LGPLv3"
             :url "http://www.gnu.org/licenses/lgpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.0.0"]])
+                 [instaparse "1.0.1"]])
