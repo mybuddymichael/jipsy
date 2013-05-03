5ae56a0d27e20c9da1b0cdca7124dde379dd5f1e
Michael Hanson <michael.b.hanson@gmail.com>
2013-05-02 20:26:34 -0700

    Make the grammar a parser function

--- src/jipsy/core.clj
+++ src/jipsy/core.clj
@@ -7,43 +7,42 @@
      (println "debug:" '~x "=" x#)
      x#))
 
-(def grammar
-  "program = form*
-   form = <ws>? definition <ws>?
-        | <ws>? assignment <ws>?
-        | <ws>? expression <ws>?
+(def parser
+  (instaparse/parser
+    "program = form*
+     form = <ws>? definition <ws>?
+          | <ws>? assignment <ws>?
+          | <ws>? expression <ws>?
 
-   definition = <'(defn'> <ws>+ identifier <ws>+ argslist <ws>+ expression <')'>
-   argslist = <'['> <ws>* identifier* <ws>* <']'>
+     definition = <'(defn'> <ws>+ identifier <ws>+ argslist <ws>+ expression <')'>
+     argslist = <'['> (<ws>* identifier)* <ws>* <']'>
 
-   assignment = <'(def'> <ws>+ identifier (<ws>+ expression)? <ws>* <')'>
+     assignment = <'(def'> <ws>+ identifier (<ws>+ expression)? <ws>* <')'>
 
-   <expression> = number
-                | string
-                | object
-                | array
-                | identifier
-                | call
-                | comment
+     expression = number
+                  | string
+                  | object
+                  | array
+                  | identifier
+                  | call
+                  | comment
 
-   call = <'('> <ws>* identifier (<ws>+ expression)* <ws>* <')'>
-        | <'('> <ws>* (addop | mulop) <ws>+ expression (<ws>+ expression)* <ws>* <')'>
-        | <'('> <ws>* (expop | modop) <ws>+ expression <ws>+ expression (<ws>+ expression)* <ws>* <')'>
+     call = <'('> <ws>* identifier (<ws>+ expression)* <ws>* <')'>
+          | <'('> <ws>* (addop | mulop) <ws>+ expression (<ws>+ expression)* <ws>* <')'>
+          | <'('> <ws>* (expop | modop) <ws>+ expression <ws>+ expression (<ws>+ expression)* <ws>* <')'>
 
-   number = #'[0-9]+([.][0-9]+)?'
-   string = #'\"[^\"]*\"'
-   object = <'{'> (<ws>* keyword <ws>+ expression <ws>*)* <'}'>
-   array = <'['> <ws>* expression* <ws>* (<ws>+ expression <ws>*)* <']'>
+     number = #'[0-9]+([.][0-9]+)?'
+     string = #'\"[^\"]*\"'
+     object = <'{'> (<ws>* keyword <ws>+ expression <ws>*)* <'}'>
+     array = <'['> <ws>* expression* <ws>* (<ws>+ expression <ws>*)* <']'>
 
-   keyword = <':'> identifier
-   identifier = !reserved #'[A-Za-z][A-Za-z0-9_]*'
-   addop = '+' | '-'
-   mulop = '*' | '/'
-   expop = '^'
-   modop = '%'
-   reserved = 'def' | 'fn'
+     keyword = <':'> identifier
+     identifier = !reserved #'[A-Za-z][A-Za-z0-9_]*'
+     addop = '+' | '-'
+     mulop = '*' | '/'
+     expop = '^'
+     modop = '%'
+     reserved = 'def' | 'fn'
 
-   comment = <#';+'> <ws>* #'[^\\s].*$'
-   ws = #'[\\s,]'")
-
-(def parser (instaparse/parser grammar))
+     comment = <#';+'> <ws>* #'[^\\s].*$'
+     ws = #'[\\s,]'"))
